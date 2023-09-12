62b358930109cbda9e7e9f3e252579819a458722
ParkDH0809 <znxn1237@gmail.com>
2023-09-13 00:33:58 +0900

    server, clinet 실행 분리

--- chatting/client/Client.java
+++ chatting/client/Client.java
@@ -6,11 +6,11 @@ import java.net.Socket;
 
 public class Client {
     
-    public void doit() {
+    public static void main(String[] args) {
         try {
             String serverIP = "127.0.0.1";
             Socket socket = new Socket(serverIP, 8888);
-
+            
             InputStream in = socket.getInputStream();
             DataInputStream dis = new DataInputStream(in);
 
--- chatting/server/Server.java
+++ chatting/server/Server.java
@@ -7,12 +7,15 @@ import java.net.*;
 
 public class Server {
 
-    public void doit() {
+    public static void main(String[] args) {
         ServerSocket serverSocket = null;
 
         try {
             serverSocket = new ServerSocket(8888);
+
             System.out.println("서버 준비 완료");
+            System.out.println("Port번호: " + serverSocket.getLocalPort());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -25,7 +28,7 @@ public class Server {
                 OutputStream out = socket.getOutputStream();
                 DataOutputStream dos = new DataOutputStream(out);
 
-                dos.writeUTF("[Test] from Server");
+                dos.writeUTF("[Server] 연결되었습니다.");
 
                 dos.close();
                 socket.close();
